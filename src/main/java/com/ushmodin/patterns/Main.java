b1e6bfadd3bff8bf8a4d031686252b67f3fad3b4
pavel <pavelu6@gmail.com>
2024-02-06 14:43:10 +0400

    task 1: singleton

---
+++ src/main/java/com/ushmodin/patterns/Main.java
@@ -0,0 +1,8 @@
+package com.ushmodin.patterns;
+
+public class Main {
+    public static void main(String[] args) {
+        var props = Props.getInstance();
+        System.out.println(props.toString());
+    }
+}
---
+++ src/main/java/com/ushmodin/patterns/Props.java
@@ -0,0 +1,30 @@
+package com.ushmodin.patterns;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Props {
+    static Props instance;
+    private Properties data;
+    private Props(){
+        data = new Properties();
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        try(var fis = classloader.getResourceAsStream("config.properties")){
+            data.load(fis);
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static Props getInstance(){
+        if (instance == null) {
+            instance = new Props();
+        }
+        return instance;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
