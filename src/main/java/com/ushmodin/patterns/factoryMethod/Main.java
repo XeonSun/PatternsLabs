ffc79aa3a64d2d1ca4b8aaaa59817f348616ff36
pavel <pavelu6@gmail.com>
2024-02-09 17:07:30 +0400

    fix:main prints class name in console

--- src/main/java/com/ushmodin/patterns/factoryMethod/Main.java
+++ src/main/java/com/ushmodin/patterns/factoryMethod/Main.java
@@ -5,9 +5,9 @@ import com.ushmodin.vehicle.TransportWorker;
 public class Main {
     public static void main(String[] args){
         var auto = TransportWorker.createInstance("auto",5);
-        TransportWorker.printModelsNames(auto);
+        System.out.println(auto.getClass().getName());
         TransportWorker.setFactory(new MotocycleFactory());
         var moto = TransportWorker.createInstance("moto", 5);
-        TransportWorker.printModelsNames(moto);
+        System.out.println(moto.getClass().getName());
     }
 }
